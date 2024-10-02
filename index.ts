975d48d7841873202eca3aadfac7555c7eb9a757
Mert Bora İnevi <m.b.inevi@student.tudelft.nl>
2024-10-03 02:55:39 +0300

    ievan polkka

--- index.ts
+++ index.ts
@@ -62,5 +62,14 @@ const server = polka()
         res.statusCode = 502;
         res.end(message);
     }
-})
-.listen(process.env.PORT || 3000);
+});
+
+(async () => {
+    if (process.env.SSL_CERT) {
+        server.server = (await import("node:https"))
+            .createServer({ key: process.env.SSL_KEY, cert: process.env.SSL_CERT });
+        server.listen(process.env.PORT || 3000);
+    } else {
+        server.listen(process.env.PORT || 3000);
+    }
+})();
